86157f345f111505646cf89e61b35d02955eebc3
YuXiu <35234267+yuxiu198@users.noreply.github.com>
2019-04-26 04:20:53 -0700

    Add files via upload

--- MancalaTest.java
+++ MancalaTest.java
@@ -1,27 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
 
-package MancalaProject;
+public class MancalaTest {
+    public static void main(String[] args) {
+        int[] dummyData = new int[14];
 
-public class MancalaTest 
-{
-    public static void main(String[] args) 
-    {
-    	int[] dummyData = new int[14];
-    	
-    	for(int i=0; i<14; i++)
-    	{
-    		dummyData[i] = 3;
-    	}
-    	
-    	dummyData[4] = 7;
-    	dummyData[6] = 5;
-    	dummyData[10] = 13;
-    	dummyData[13] = 20;
-    	
-    	BoardLayout test = new HorizontalLayout();
-    	test.setData(dummyData);
-    	test.drawBoard();
-        //VerticalLayout layoutV = new VerticalLayout();
+        for(int i=0; i<14; i++)
+        {
+            dummyData[i] = 2;
+        }
 
+        dummyData[4] = 7;
+        dummyData[6] = 5;
+        dummyData[10] = 13;
+        dummyData[13] = 10;
+
+        //BoardLayout test = new HorizontalLayout();
+        //test.setData(dummyData);
+        //test.drawBoard();
+
+
+        BoardLayout test2 = new VerticalLayout();
+        test2.setData(dummyData);
+        test2.drawBoard();
 
     }
 }
